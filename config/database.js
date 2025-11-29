bf4483d2a65d94bab6c153313c78c19afbcfc049
chrisAXC <akilesramirez4@gmail.com>
2025-11-28 23:09:00 -0600

    Fix database connection configuration

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('pg');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-<<<<<<< HEAD
+
     host: process.env.DB_HOST || 'dpg-d4l70fm3jp1c73971vj0-a',
     user: process.env.DB_USER || 'carrito_user',
     password: process.env.DB_PASSWORD || 'BKW5rOtirn1vdToISJgGWfCZzzMtqjWU',
