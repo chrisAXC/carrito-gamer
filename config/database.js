a4251441e4d560c45d06cfeb38e66014b1851275
chrisAXC <akilesramirez4@gmail.com>
2025-11-28 23:08:20 -0600

    Switch database connection from MySQL to PostgreSQL

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('pg');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
@@ -6,12 +6,6 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'dpg-d4l70fm3jp1c73971vj0-a',
     user: process.env.DB_USER || 'carrito_user',
     password: process.env.DB_PASSWORD || 'BKW5rOtirn1vdToISJgGWfCZzzMtqjWU',
-=======
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
->>>>>>> 665db919c4853aa95fd1148528a82988132e52f9
-    database: process.env.DB_NAME || 'carrito_gamer'
 });
 
 connection.connect((err) => {
@@ -19,7 +13,7 @@ connection.connect((err) => {
         console.error('Error conectando a la base de datos:', err);
         return;
     }
-    console.log('Conectado a la base de datos MySQL');
+    console.log('Conectado a la base de datos POSTEGRES');
 });
 
 module.exports = connection;
